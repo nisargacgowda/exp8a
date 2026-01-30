f4974c54bd1da3abd250cae40c5366f6a76e7df0
Nisarga C <nisargac2006@gmail.com>
2026-01-30 09:06:49 +0530

    Update BankService.java

--- BankService.java
+++ BankService.java
@@ -28,7 +28,7 @@ public class BankService {
  // The main entry point
 public static void main(String[] args) {
  // Create an instance of the BankAccount class
- BankAccount acc = new BankAccount(5000);
+ BankAccount acc = new BankAccount(10000);
 
  System.out.println("Initial Balance: " + acc.getBalance());
  // Perform operations
